1904f64883a4c80901fbdb9d232038fa820365bb
jiwanbang <jiwanbang@gmail.com>
2024-09-24 04:28:34 +0900

    feat: project_base

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Bg from "./Component/Bg";
+
+function App() {
+  return (
+    <div className="">
+      <Bg />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Component/Bg.tsx
@@ -0,0 +1,11 @@
+import bgimg from "../img/5ef26b5363fff815b5dad77752e3980f99d0a908-2000x3000.webp";
+
+const Bg = () => {
+  return (
+    <div>
+      <img className="w-full h-full fixed" src={bgimg} />;
+    </div>
+  );
+};
+
+export default Bg;
